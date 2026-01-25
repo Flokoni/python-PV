e44563e419dc674d7c1bb413e29c7827697b84e7
Viktor <v_purits@kbtu.kz>
2026-01-25 12:10:51 +0500

    strings commit

---
+++ Practise1/python-basics/Python Strings/Python - Escape Characters.py
@@ -0,0 +1,18 @@
+txt = "We are the so-called \"Vikings\" from the north."
+txt = 'It\'s alright.'
+print(txt) 
+
+
+txt = "Hello\nWorld!"
+print(txt) 
+
+
+txt = "\110\145\154\154\157"
+print(txt) 
+
+
+txt = "\x48\x65\x6c\x6c\x6f"
+print(txt) 
+
+txt = "Hello\tWorld!"
+print(txt) 
---
+++ Practise1/python-basics/Python Strings/Python - String Methods.py
@@ -0,0 +1,34 @@
+txt = "hello, and welcome to my world."
+
+x = txt.capitalize()
+
+print (x)
+
+txt = "banana"
+
+x = txt.center(20)
+
+print(x)
+
+txt = "Hello, welcome to my world."
+
+x = txt.find("welcome")
+
+print(x)
+
+txt = "hello world!"
+
+x = txt.islower()
+
+print(x)
+
+txt = "Hello my FRIENDS"
+
+x = txt.lower()
+
+print(x)
+txt = "apple, banana, cherry"
+
+x = txt.rsplit(", ")
+
+print(x)
