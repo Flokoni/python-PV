e99d3a5f20c750e054388bd1acaffd6a8ed8ee0a
Viktor <v_purits@kbtu.kz>
2026-01-28 15:44:45 +0500

    boolean end

---
+++ Practise2/Boolean Operators.py
@@ -0,0 +1,30 @@
+
+
+has_login = True
+has_password = True
+
+can_login = has_login and has_password
+print(f"Доступ разрешен: {can_login}") # True
+
+is_weekend = True
+is_holiday = False
+
+
+can_rest = is_weekend or is_holiday
+print(f"Can we rest? {can_rest}") # True
+
+is_engine_running = False
+
+print(f"Are we need to run engine? {not is_engine_running}") 
+
+score = 85
+
+is_top_score = score > 80 and score <= 100
+print(f"It is a good mark? {is_top_score}") # True
+
+is_admin = False
+has_key = True
+is_owner = False
+
+access = (is_admin or has_key) and not is_owner
+print(f"access is allowed: {access}") # True
